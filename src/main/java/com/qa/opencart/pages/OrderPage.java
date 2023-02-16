8dbcbbcdc0928b4c4e7d725b34cf577a4151080d
Pravin Junghare <pravinjunghare01@gmail.com>
2023-02-16 17:16:40 +0530

    Changes done as per comments

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -1,10 +1,15 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class OrderPage
 
 {
-	public void order()
-	{
+  private By name =By.id(("name"));
+	
+	
+	
+	public void orderFeature() {
 		System.out.println("OrderPage Added");
 	}
 
